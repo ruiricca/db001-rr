a69fc2bde729883327f36f9536f26af6aba9e280
ruiricca <ruiricca@gmail.com>
2025-11-14 10:40:36 +0000

    Create create-autores.sql

---
+++ create-autores.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `autores` (
+  `autor_id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `nif` varchar(45) NOT NULL,
+  PRIMARY KEY (`autor_id`),
+  UNIQUE KEY `nif_UNIQUE` (`nif`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
